c4bf814bc116a549a8935d28c64407046632f1c2
Nazmul Huq <nazmulhuq411@gmail.com>
2019-04-01 11:09:58 +0600

    Anonymous Chat

---
+++ ChatApp/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatApp
+{
+    public class ChatHub : Hub
+    {
+        public void Announce(string message)
+        {
+           
+            Clients.Others.Announce(message);
+            
+        }
+    }
+}
---
+++ ChatApp/Scripts/chatWindow.js
@@ -0,0 +1,21 @@
+﻿(function () {
+
+    $.connection.hub.start()
+        .done(function () {
+            console.log("Connected");
+        })
+        .fail(function () { alert("Error..."); });
+
+    $("#click-me").click(function () {
+        var sentMessage = $("#message").val();
+
+        $.connection.chatHub.server.announce(sentMessage);
+        $("#allMessages").append(sentMessage + "<br />");
+        $("#message").val("");
+    })
+
+    $.connection.chatHub.client.announce = function (message) {
+        $("#allMessages").append(message + "<br />");
+    }
+
+})()
